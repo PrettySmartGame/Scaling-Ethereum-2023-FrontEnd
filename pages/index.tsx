fc548dbecaebb4537d44ee0999eb8e3445f08cd4
Julio M Cruz <julio.cruz@eb-ms.net>
2023-03-16 19:36:21 -0400

    Landing Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,91 +2,129 @@ import { ConnectButton } from '@rainbow-me/rainbowkit';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
-import {
-  Box,
-  Heading,
-  Container,
-  Text,
-  Button,
-  Stack,
-  Icon,
-  useColorModeValue,
-  createIcon,
-} from '@chakra-ui/react';
+import { Avatar, Badge, Box, Button, Container, VStack, HStack, StackDivider, Stack, Text, IconButton } from '@chakra-ui/react'
 
-const Home: NextPage = () => {
-  return (
-    <>
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Caveat:wght@700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
+import { FiEdit2, FiTrash2 } from 'react-icons/fi'
+import { experiences } from '../data/data'
 
-      <Container maxW={'3xl'}>
-        <Stack
-          as={Box}
-          textAlign={'center'}
-          spacing={{ base: 8, md: 14 }}
-          py={{ base: 20, md: 36 }}>
-          <Heading
-            fontWeight={600}
-            fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
-            lineHeight={'110%'}>
-            Here is the Title <br />
-            <Text as={'span'} color={'green.400'}>
-              Sub-Title Here
-            </Text>
-          </Heading>
-          <Text color={'gray.500'}>
-          Fusce hendrerit, est a aliquam elementum, augue magna maximus tellus, sit amet pretium magna mauris eget purus. Curabitur quis fringilla orci, eget imperdiet erat. Nullam rhoncus et libero sed fermentum. Maecenas consequat in risus et dignissim. Duis ornare metus nec odio iaculis ultrices. Ut sagittis quis massa rutrum gravida. Vestibulum non nibh mattis, commodo arcu nec, mollis augue.
-          </Text>
-          <Stack
-            direction={'column'}
-            spacing={3}
-            align={'center'}
-            alignSelf={'center'}
-            position={'relative'}>
-              <ConnectButton />
-              <Box>
-              <Icon
-                as={Arrow}
-                color={useColorModeValue('gray.800', 'gray.300')}
-                w={71}
-                position={'absolute'}
-                right={-71}
-                top={'10px'}
-              />
-              <Text
-                fontSize={'lg'}
-                fontFamily={'Caveat'}
-                position={'absolute'}
-                right={'-125px'}
-                top={'-15px'}
-                transform={'rotate(10deg)'}>
-                Login Here
-              </Text>
+const Landing: NextPage = () => {
+
+    return (
+        <>
+            <Box as="section" pt={{ base: '4', md: '8' }} pb={{ base: '12', md: '24' }}
+                bgGradient={[
+                    'linear(to-tr, teal.300, yellow.400)',
+                    'linear(to-t, blue.200, teal.500)',
+                    'linear(to-b, orange.100, purple.300)',
+                  ]}>
+                <Container
+                    w='100%'
+                    bgGradient='linear(to-r, gray.300, yellow.400, pink.200)'
+                    borderTopRadius={'xl'}
+                    >
+
+
+                    <VStack
+                        divider={<StackDivider borderColor='gray.200' />}
+                        spacing={4}
+                        align='stretch'
+                        width={{ base: 'full', md: 'md' , lg: 'lg', xl: 'xl'}}
+                        >
+                        <Box
+                            bg="bg-surface"
+                            px={{ base: '4', md: '6' }}
+                            pt="5"
+                            boxShadow="sm"
+                            borderColor="accent"
+                            >
+                            <Stack spacing="4" direction={{ base: 'column', sm: 'row' }} justify="space-between">
+                                <HStack spacing="4">
+                                <Avatar
+                                    src="https://tinyurl.com/yhkm2ek8"
+                                    name="Christoph Winston"
+                                    boxSize={{ base: '12', sm: '14' }}
+                                />
+                                <Box>
+                                    <HStack>
+                                    <Text fontSize="lg" fontWeight="medium">
+                                        Christoph Winston
+                                    </Text>
+                                    <Badge variant="subtle" colorScheme="cyan">
+                                        Verified
+                                    </Badge>
+                                    </HStack>
+                                    <Text color="muted" fontSize="sm">
+                                    user@ETHGlobal.com
+                                    </Text>
+                                </Box>
+                                </HStack>
+                            </Stack>
+                        </Box>
+                        <Box bg="bg-surface"
+                            px={{ base: '4', md: '6' }}
+                            pb="5"
+                            boxShadow="sm"
+                            borderColor="accent"
+                            >
+                            <Stack direction="row" spacing="3">
+                                <ConnectButton />
+                            </Stack>
+                        </Box>
+                    </VStack>  
+
+
+                </Container>
+
+                {/* <Container maxW="3xl"></Container> */}
+                <Container
+                    w='100%'
+                    borderBottomRadius={'xl'}
+                    bgGradient='linear(to-r, #F6F0EA, #F1DFD1)'
+                    >
+                <Box bg="bg-surface" boxShadow="sm" borderRadius="lg" p={{ base: '4', md: '6' }}>
+                    <Stack spacing="5" divider={<StackDivider />}>
+                    <Stack justify="space-between" direction={{ base: 'column', sm: 'row' }} spacing="5">
+                        <Stack spacing="1">
+                        <Text fontSize="lg" fontWeight="medium">
+                            Experiences
+                        </Text>
+                        <Text fontSize="sm" color="muted">
+                            Write in a few short sentences where you have already worked.
+                        </Text>
+                        </Stack>
+                        <Button variant="primary">Add</Button>
+                    </Stack>
+                    {experiences.map((experience, id) => (
+                        <Stack key={id} justify="space-between" direction="row" spacing="4">
+                        <Stack spacing="0.5" fontSize="sm">
+                            <Text color="emphasized" fontWeight="medium">
+                            {experience.title}
+                            </Text>
+                            <Text color="muted">{experience.description}</Text>
+                        </Stack>
+
+                        <Stack direction={{ base: 'column', sm: 'row' }} spacing={{ base: '0', sm: '1' }}>
+                            <IconButton
+                            icon={<FiEdit2 fontSize="1.25rem" />}
+                            variant="ghost"
+                            aria-label="Edit experience"
+                            />
+                            <IconButton
+                            icon={<FiTrash2 fontSize="1.25rem" />}
+                            variant="ghost"
+                            aria-label="Delete experience"
+                            />
+                        </Stack>
+                        </Stack>
+                    ))}
+                    </Stack>
+                </Box>
+                </Container>
+
             </Box>
-          </Stack>
-        </Stack>
-      </Container>
-    </>
 
-  );
+        </>
+    );
 };
 
-const Arrow = createIcon({
-  displayName: 'Arrow',
-  viewBox: '0 0 72 24',
-  path: (
-    <path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M0.600904 7.08166C0.764293 6.8879 1.01492 6.79004 1.26654 6.82177C2.83216 7.01918 5.20326 7.24581 7.54543 7.23964C9.92491 7.23338 12.1351 6.98464 13.4704 6.32142C13.84 6.13785 14.2885 6.28805 14.4722 6.65692C14.6559 7.02578 14.5052 7.47362 14.1356 7.6572C12.4625 8.48822 9.94063 8.72541 7.54852 8.7317C5.67514 8.73663 3.79547 8.5985 2.29921 8.44247C2.80955 9.59638 3.50943 10.6396 4.24665 11.7384C4.39435 11.9585 4.54354 12.1809 4.69301 12.4068C5.79543 14.0733 6.88128 15.8995 7.1179 18.2636C7.15893 18.6735 6.85928 19.0393 6.4486 19.0805C6.03792 19.1217 5.67174 18.8227 5.6307 18.4128C5.43271 16.4346 4.52957 14.868 3.4457 13.2296C3.3058 13.0181 3.16221 12.8046 3.01684 12.5885C2.05899 11.1646 1.02372 9.62564 0.457909 7.78069C0.383671 7.53862 0.437515 7.27541 0.600904 7.08166ZM5.52039 10.2248C5.77662 9.90161 6.24663 9.84687 6.57018 10.1025C16.4834 17.9344 29.9158 22.4064 42.0781 21.4773C54.1988 20.5514 65.0339 14.2748 69.9746 0.584299C70.1145 0.196597 70.5427 -0.0046455 70.931 0.134813C71.3193 0.274276 71.5206 0.70162 71.3807 1.08932C66.2105 15.4159 54.8056 22.0014 42.1913 22.965C29.6185 23.9254 15.8207 19.3142 5.64226 11.2727C5.31871 11.0171 5.26415 10.5479 5.52039 10.2248Z"
-      fill="currentColor"
-    />
-  ),
-});
-
-export default Home;
+export default Landing;
